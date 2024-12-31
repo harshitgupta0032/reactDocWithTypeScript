a84a290b5d2f942b87a875840522338908ef3e32
harshitgupta003 <harshitkumargupta03.hp@gmail.com>
2024-12-31 17:54:19 +0530

    fix vercel error

--- src/Component/Hooks/UseEffectHooks/UseEffectHooks.tsx
+++ src/Component/Hooks/UseEffectHooks/UseEffectHooks.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import SelectOptions from "./SelectOptions";
 import Chat from "./SearchChat/Chat";
-import PointerMove from "./PointerMove/PointerMove";
 
 const UseEffectHooks: React.FC = () => {
     const [OptionValue, setOptionValue] = useState<string>("genral");
