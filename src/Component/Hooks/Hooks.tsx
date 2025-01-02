820b760f46db95952264c71c6cc4f0acca3b8fdb
harshitgupta003 <harshitkumargupta03.hp@gmail.com>
2025-01-02 17:24:40 +0530

    fix name of all pages

--- src/Component/Hooks/Hooks.tsx
+++ src/Component/Hooks/Hooks.tsx
@@ -1,29 +1,29 @@
 // import CustomHooks from "./CustomHooks/CustomHooks";
-import CounterContextHooks from "./UseContextHook/CounterContextHooks";
-import ThemeContext from "./UseContextHook/ThemeContext";
-import UseContextHook from "./UseContextHook/UseContextHooks";
-import UseEffectHooks from "./UseEffectHooks/UseEffectHooks";
-import UseReducerHooks from "./UseReducerHooks/UseReducerHooks";
-import UseRefHooks from "./UseRefHooks/UseRefHooks";
-import UseStateHooks from "./UseStateHook/UseStateHooks";
+import CounterContextHooks from "./ContextHook/CounterContextHooks";
+import ThemeContext from "./ContextHook/ThemeContext";
+import ContextHook from "./ContextHook/ContextHooks";
+import EffectHooks from "./EffectHooks/EffectHooks";
+import ReducerHooks from "./ReducerHooks/ReducerHooks";
+import RefHooks from "./RefHooks/RefHooks";
+import StateHooks from "./StateHook/StateHooks";
 
 const Hooks: React.FC = () => {
     return <>
         <div className='bg-gray-300 min-h-screen h-fit pb-6 w-full flex flex-col  pt-10 items-center gap-6'>
             <h1 className="text-black text-xl font-bold">Hooks With TypeScript</h1>
 
-            <UseStateHooks />
-            <UseEffectHooks />
+            <StateHooks />
+            <EffectHooks />
 
             {/* start useContext using child */}
             <ThemeContext>
                 <CounterContextHooks>
-                    <UseContextHook /> {/* this is main component it access all context */}
+                    <ContextHook /> {/* this is main component it access all context */}
                 </CounterContextHooks>
             </ThemeContext>
 
-            <UseRefHooks />
-            <UseReducerHooks/>
+            <RefHooks />
+            <ReducerHooks />
 
             {/* <CustomHooks /> */}
         </div>
--- src/Component/Hooks/RefHooks/InputFocus.tsx
+++ src/Component/Hooks/RefHooks/InputFocus.tsx
@@ -22,7 +22,7 @@ const InputFocus: React.FC = () => {
     return <>
         <div className="flex flex-col gap-4">
             <div className="flex justify-center">
-                <input ref={FocusRef} type="text" value={textInput} placeholder="Click button for direct Focus " onChange={HandleInput} className="outline-black border-2 border-gray-400 rounded-xl text-black px-4 w-72 sm:w-80 md:w-96 py-2 md:py-3" />
+                <input ref={FocusRef} type="text" value={textInput} placeholder="Click button for direct Focus " onChange={HandleInput} className="outline-black outline-8 border-2 border-gray-400 rounded-xl text-black px-4 w-72 sm:w-80 md:w-96 py-2 md:py-3" />
             </div>
             <div className="flex flex-wrap justify-center items-center gap-4">
                 <button className="border-2 border-gray-500 rounded-lg px-3 py-1 active:bg-gray-500 active:text-white" onClick={FocusOn}>Focus on</button>
