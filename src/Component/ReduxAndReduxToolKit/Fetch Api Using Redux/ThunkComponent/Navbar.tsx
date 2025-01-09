b196f0e1be100fa06056ae3e7b8cf195261741fb
harshitgupta0032 <harshitkumargupta03.hp@gmail.com>
2025-01-09 10:57:34 +0530

    fix z index in navbar and also giv transistion on slidebutton

--- src/Component/ReduxAndReduxToolKit/Fetch Api Using Redux/ThunkComponent/Navbar.tsx
+++ src/Component/ReduxAndReduxToolKit/Fetch Api Using Redux/ThunkComponent/Navbar.tsx
@@ -1,16 +1,19 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { SliderStatus } from "../../Redux/feature/ReduxThunk/ReduxSlideSlice";
 
 const Navbar = () => {
     const dispatch = useDispatch();
-    return <>
-        <div className="fixed bg-white z-10  h-20 shadow-lg w-full justify-between px-5 items-center flex">
-            <div className="h-fit flex justify-center items-center gap-5 w-fit">
+    const slidestatus = useSelector((state: any) => state.slider.slidestatus);
 
-                <svg onClick={() => dispatch(SliderStatus())} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="cursor-pointer size-7 flex sm:hidden">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
-                </svg>
+    return <>
+        <div className="fixed bg-white z-50  h-20 shadow-lg w-full justify-between px-5 items-center flex">
+            <div className="h-fit flex justify-center items-center gap-5 w-fit">
+                <div onClick={() => dispatch(SliderStatus())} className="w-6 h-6 transition-all duration-500 ease-in-out relative flex justify-center items-center cursor-pointer flex-col gap-1">
+                    <div className={`h-[3px] w-5 bg-black rounded-full transition-transform duration-700  ease-in-out ${slidestatus ? "rotate-[45deg] absolute " : ""}`}></div>
+                    <div className={`h-[3px] w-5 bg-black rounded-full transition-opacity  ${slidestatus ? "opacity-0" : ""}`}></div>
+                    <div className={`h-[3px] w-5 bg-black rounded-full transition-transform duration-700  ease-in-out ${slidestatus ? "rotate-[-45deg] absolute" : ""}`}></div>
+                </div>
 
                 <Link to={""}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44.36 48.82" className="h-9 w-9">
@@ -32,7 +35,7 @@ const Navbar = () => {
                 <input type="text" placeholder="Search the product" className="h-7 w-80 outline-none" />
             </div>
             <div className="flex  gap-10">
-                {/* <div className="h-fit w-fit"> */}
+
                 <Link to={""} className="relative">
                     <div className="min-h-5 min-w-5 absolute top-[-16px] left-[-12px] flex justify-center items-center bg-red-500 rounded-full text-sm text-white">1
                     </div>
@@ -40,7 +43,7 @@ const Navbar = () => {
                         <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                     </svg>
                 </Link>
-                {/* </div> */}
+
                 <Link to={""}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
--- src/Component/ReduxAndReduxToolKit/Fetch Api Using Redux/ThunkComponent/SideSlideBar.tsx
+++ src/Component/ReduxAndReduxToolKit/Fetch Api Using Redux/ThunkComponent/SideSlideBar.tsx
@@ -9,9 +9,9 @@ interface SlideCategoryType {
 const SideSlideBar: React.FC<SlideCategoryType> = ({ dataCategory, setCategoryStatus, categorieStatus }) => {
     const slidestatus = useSelector((state: any) => state.slider.slidestatus);
 
-    return <div className={`${slidestatus ? "block" : "hidden sm:block sm:pt-20"}  h-screen w-48 pt-20 sm:pt-0  bg-blue-100 fixed sm:sticky top-0 left-0 bottom-0`}>
+    return <div className={`${slidestatus ? "block" : "hidden sm:block sm:pt-20"} z-10  h-screen w-48 pt-20 sm:pt-0  bg-blue-100 fixed sm:sticky top-0 left-0 bottom-0`}>
 
-        <div className="flex w-full pt-20 flex-col  justify-center items-center gap-6">
+        <div className="flex w-full pt-20 flex-col   justify-center items-center gap-6">
             <h1 className="font-bold text-lg">Category</h1>
             <ul className="flex  w-full justify-center items-center flex-col gap-3">
                 <li onClick={() => setCategoryStatus("all")} className={`cursor-pointer py-2  flex  w-full justify-center items-center hover:bg-blue-200 font-semibold ${categorieStatus === "all" ? "text-orange-500 hover:text-orange-700" : "hover:text-blue-500 text-black"}`}>All</li>
